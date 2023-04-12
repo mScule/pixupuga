094c2c72190e86261cf0990556db959138bc73df
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2023-04-13 00:06:23 +0300

    Add level 11

--- src/assets/level/jungle/10.ts
+++ src/assets/level/jungle/10.ts
@@ -2,12 +2,12 @@ import type Level from "../../../lib/types/Level"
 import LevelWorld from "../../../lib/types/LevelWorld";
 
 export default {
-  name: "Jungleana Jones",
-  winningPoints: 10,
+  name: "Cursed village",
+  winningPoints: 5,
   world: LevelWorld.Jungle,
   lower: {
     "_": "0 1 2 3 4 5 6 7 8 9 A B C D E F",
-    "0": "s s s s s s c s s c s s c s s s",
+    "0": "s s s s s s s s s s s s s s s s",
     "1": "s s s s s s s s s s s s s s s s",
     "2": "s s s s s s s s s s s s s s s s",
     "3": "s s s s s s s s s s s s s s s s",
@@ -22,25 +22,25 @@ export default {
     "C": "s s s s s s s s s s s s s s s s",
     "D": "s s s s s s s s s s s s s s s s",
     "E": "s s s s s s s s s s s s s s s s",
-    "F": "s s s s s s c s s c s s c s s s"
+    "F": "s s s s s s s s s s s s s s s s"
   },
   upper: {
     "_": "0 1 2 3 4 5 6 7 8 9 A B C D E F",
-    "0": "s s s s s s . s s . s s . s s s",
-    "1": "s s s s s s . s s r s s . s B s",
-    "2": "s s s s s s . . . . s s . . . s",
-    "3": "s s s s s s . s s . s s . s s s",
-    "4": "s s s s s s . s s . s s . s s s",
-    "5": "s s s s s s . s s . s s . s s s",
-    "6": "s s s s s s . s s . s s . s s s",
-    "7": "s s s s s s . s s . s s . s s s",
-    "8": "s s s s s s . s s . s s . s s s",
-    "9": "s s s s s s . s s . s s . s s s",
-    "A": "s s s s s s . s s . s s . s s s",
-    "B": "s s s s s s . s s . s s . s s s",
-    "C": "s s s s s s . s s . s s r s s s",
-    "D": "s s s s s s r s s . . . . s s s",
-    "E": "s s s s p . . s s . s s . s s s",
-    "F": "s s s s s s . s s . s s . s s s"
+    "0": ". . . . . . . . . . . . . . . .",
+    "1": ". . . . . . . . . . . . . . . .",
+    "2": ". . . . . . . . . . . . . . . .",
+    "3": ". . . . . . . . . . . . . . . .",
+    "4": ". . . . . . . . . . . . . . . .",
+    "5": ". . . . . . . . . . . . . . . .",
+    "6": ". . . . . . . . . . . . . . . .",
+    "7": ". . . . . . . p . . . . . . . .",
+    "8": ". . . . . . . . . . . . . . . .",
+    "9": ". . . . . . . . . . . . . . . .",
+    "A": ". . . . . . . . . . . . . . . .",
+    "B": ". . . . . . . . . . . . . . . .",
+    "C": ". . . . . . . . . . . . . . . .",
+    "D": ". . . . . . . . . . . . . . . .",
+    "E": ". . . . . . . . . . . . . . . .",
+    "F": ". . . . . . . . . . . . . . . ."
   }
 } as Level;
