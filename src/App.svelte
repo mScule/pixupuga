992ec0b66195a00bcbeaa781069bf9d4966b98db
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2023-04-13 19:42:16 +0300

    Add level 15

--- src/App.svelte
+++ src/App.svelte
@@ -52,7 +52,7 @@
     ];
 
     let activeView: ActiveView = ActiveView.Game;
-    let selectedLevel: number = 14 -1;
+    let selectedLevel: number = 15 -1;
 
     function handleStart() {
         activeView = ActiveView.LevelSelection;
